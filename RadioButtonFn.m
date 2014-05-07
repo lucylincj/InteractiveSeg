68b89b8523ce925d10fc8206645eafef777d2e94
U-lucylin-PC\lucylin <lucylin@lucylin-PC.(none)>
2014-05-07 15:30:17 +0800

    add function

---
+++ RadioButtonFn.m
@@ -0,0 +1,25 @@
+function RadioButtonFn(source, eventdata)
+% RADIOBUTTONFUNCTION - This function is called whenever there is change in
+% choice of radio button
+
+% Global Variables
+global hfig longfilename;
+
+% Pass string value to seed selection function
+strg = get(eventdata.NewValue,'String');
+
+%%%%%%%%%%%%%%%%Seed Selection Buttons%%%%%%%%%%%%%%%%%%%
+% Calls fginput - gets foreground pixels from the user
+data.ui.push_fg = uicontrol(hfig, 'Style','pushbutton', 'Units', 'Normalized','Position',[.7 .6 .1 .05], ...
+    'String','Foreground','Callback', ['fginput ',strg]);
+
+% Calls bginput - gets background pixels from the user
+data.ui.push_bg = uicontrol(hfig, 'Style','pushbutton', 'Units', 'Normalized','Position',[.7 .5 .1 .05], ...
+    'String','Background','Callback', ['bginput ',strg]);
+
+% Calls Segment - graph-cuts on the image
+data.ui.push_bg = uicontrol(hfig, 'Style','pushbutton', 'Units', 'Normalized','Position',[.7 .4 .1 .05], ...
+    'String','Graph Cuts','Callback', ['Segment ',longfilename]);
+drawnow;
+
+end
---
+++ updateMinDis.m
@@ -0,0 +1,11 @@
+function [dF dB] = updateMinDis(uncertain, numSegments, mC, fSeg, bSeg)
+    dF = zeros(1, numSegments);
+    dB = zeros(1, numSegments);
+    for i = 1:size(uncertain, 1)
+        j = uncertain(i);
+        seg = repmat(mC(:,j ), 1, numSegments);
+        dis = sum( (mC - seg).^2, 1 );
+        dF(j) = sqrt(min(dis(fSeg==1)));
+        dB(j) = sqrt(min(dis(bSeg==1)));
+    end
+end
