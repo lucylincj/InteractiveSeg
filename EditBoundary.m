c28aef18ef153710b06d11dd36d0b5a6aabd37c4
jttte <lucylin0821@gmail.com>
2014-05-09 01:54:40 +0800

    complete EditBoundary step 1

--- EditBoundary.m
+++ EditBoundary.m
@@ -1,15 +1,20 @@
 function EditBoundary(oriImg, segments, lab, editfgpixels, editbgpixels)
-    
-    FY = editfgpixels(:,1);
-    FX = editfgpixels(:,2);
-
-    BY = editbgpixels(:,1);
-    BX = editbgpixels(:,2);
-    for i = 1:size(FX, 1)
-        lab(segments(FX(i), FY(i))+1) = 1;
+    sizef = size(editfgpixels, 1);
+    sizeb = size(editbgpixels, 1);
+    if(sizef>0)
+        FY = editfgpixels(:,1);
+        FX = editfgpixels(:,2);
+        for i = 1:sizef
+            lab(segments(FX(i), FY(i))+1) = 2;
+        end
     end
-    for i = 1:size(BX, 1)
-        lab(segments(BX(i), BY(i))+1) = 0;
+    
+    if(sizeb>0)
+        BY = editbgpixels(:,1);
+        BX = editbgpixels(:,2);
+        for i = 1:sizeb
+            lab(segments(BX(i), BY(i))+1) = 1;
+        end 
     end
     drawResults(oriImg, segments, lab);
     
