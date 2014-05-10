f58e147118ae05954d189793f0b93ebd110559ca
jttte <lucylin0821@gmail.com>
2014-05-10 17:27:25 +0800

    fix problem

--- updateMinDis.m
+++ updateMinDis.m
@@ -1,7 +1,7 @@
 function [dF dB] = updateMinDis(uncertain, numSegments, mC, fSeg, bSeg)
     dF = zeros(1, numSegments);
     dB = zeros(1, numSegments);
-    for i = 1:size(uncertain, 1)
+    for i = 1:size(uncertain, 2)
         j = uncertain(i);
         seg = repmat(mC(:,j ), 1, numSegments);
         dis = sum( (mC - seg).^2, 1 );
