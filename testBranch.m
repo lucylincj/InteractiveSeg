30617924821f08b9cc199876fa23fea28818e8db
jttte <lucylin0821@gmail.com>
2014-05-12 22:05:12 +0800

    test kmeans method

--- testBranch.m
+++ testBranch.m
@@ -22,25 +22,27 @@ function testBranch()
     left = min(y1);
     right = max(y1);
     
-%     cropImg = oriImg(top:bottom, left:right,:);
-%     tmp = (segments ==347);
-%     mask = tmp(top:bottom, left:right);
-%     cform = makecform('srgb2lab');
-%     labImg = applycform(cropImg,cform);
-    
-%     ab = double(labImg(:,:,2:3));
-%     ab(:,:,1) = ab(:,:,1).*mask;
-%     ab(:,:,2) = ab(:,:,2).*mask;
-%     nrows = size(ab,1);
-%     ncols = size(ab,2);
-%     ab = reshape(ab,nrows*ncols,2);
+    cropImg = oriImg(top:bottom, left:right,:);
+    tmp = (segments ==347);
+    mask = tmp(top:bottom, left:right);
+    figure; imshow(cropImg);
     cform = makecform('srgb2lab');
-    labImg = applycform(oriImg,cform);
-    layer1 = labImg(:,:,2);
-    layer2 = labImg(:,:,3);
-    ab = zeros(2, size(x1,1));
-    ab(1, :) = double(layer1(segments==347));
-    ab(2, :) = double(layer2(segments==347));
+    labImg = applycform(cropImg,cform);
+    
+    ab = double(labImg(:,:,2:3));
+    %ab(:,:,1) = ab(:,:,1).*mask;
+    %ab(:,:,2) = ab(:,:,2).*mask;
+    nrows = size(ab,1);
+    ncols = size(ab,2);
+    ab = reshape(ab,nrows*ncols,2)';
+
+%     cform = makecform('srgb2lab');
+%     labImg = applycform(oriImg,cform);
+%     layer1 = labImg(:,:,2);
+%     layer2 = labImg(:,:,3);
+%     ab = zeros(2, size(x1,1));
+%     ab(1, :) = double(layer1(segments==347));
+%     ab(2, :) = double(layer2(segments==347));
 
     nColors = 2;
     % repeat the clustering 3 times to avoid local minima
@@ -48,7 +50,14 @@ function testBranch()
     %                                      'Replicates',3);
     addpath(genpath('vlfeat-0.9.18')) ;
     vl_setup();
+    
     [idx_, C_, e] = vl_kmeans(ab, nColors, 'verbose') ;
+    result = reshape(C_, nrows, ncols)';
+    %result = double(result) .* double(mask);
+    figure; imagesc(result);
+    
+     
+    
     
     
 end
